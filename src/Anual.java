d51c225d291e4a26a26d01a39a57691790b1e7bf
leticiamoural <leticiamolopes@hotmail.com>
2022-11-11 17:50:07 -0300

    atualização

--- src/Anual.java
+++ src/Anual.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public enum Anual {
+public enum Anual {
+    JANUARY (1000F),
+    FEBRUARY(2000F),
+    MARCH(3000F),
+    APRIL (4000F),
+    MAY(5000F),
+    JUNE(6000F),
+    JULY (7000F),
+    AUGUST(8000F),
+    SEPTEMBER(9000F),
+    OCTOBER (10000F),
+    NOVEMBER(11000F),
+    DECEMBER(12000F);
+
+    private float valor;
+
+    Anual(float valor){
+        this.valor = valor;
+    }
+
+    public float getValor(){
+        return valor;
+    }
+    public static float getValorEspecifico(String month){
+        Anual anual = Anual.valueOf(month);
+        return anual.valor;
+    }
 }
+
--- src/ValorGastoMes.java
+++ src/ValorGastoMes.java
@@ -1,5 +1,17 @@
-public class Main {
+import java.util.Scanner;
+
+public class ValorGastoMes {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //recebe valor do mês
+        System.out.println("Write a month: (January/February/March/April/May/June/July/August/September/October/November/December)");
+        Scanner input = new Scanner(System.in);
+        String monthRequired = input.next();
+
+        String monthAdapted = monthRequired.toUpperCase();
+
+        float valor = Anual.getValorEspecifico(monthAdapted);
+        System.out.println(monthAdapted +" = R$"+valor);
     }
 }
